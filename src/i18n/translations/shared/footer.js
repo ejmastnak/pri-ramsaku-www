49f31542bec159ea5b2bbafd31a698cd88321c7d
Elijan Mastnak <git@ejmastnak.com>
2025-11-25 12:58:25 +0100

    feat: add Footer component

---
+++ src/i18n/translations/shared/footer.js
@@ -0,0 +1,14 @@
+export default {
+  home: {
+    en: "Home",
+    sl: "Glavna stran",
+  },
+  about: {
+    en: "About us",
+    sl: "O nas",
+  },
+  products: {
+    en: "Products",
+    sl: "Izdelki",
+  },
+}
