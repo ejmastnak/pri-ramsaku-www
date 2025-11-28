0f8b2cdcbb969c0a7d60c8f64efd035c5c4d4e0c
Elijan Mastnak <git@ejmastnak.com>
2025-11-28 18:36:05 +0100

    feat: add Bio component

---
+++ src/i18n/translations/shared/bio.js
@@ -0,0 +1,14 @@
+export default {
+  heading: {
+    en: "Never sprayed or treated",
+    sl: "Nikoli špricano",
+  },
+  text: {
+    en: "We do not use, and never have used, any form of pesticides, herbicides, or other synthetic agrochemicals on our farm. Although we aren't enrolled in an official BIO certification program, our products are organic all but in name.",
+    sl: "Na kmetiji ne uporabljamo in nikoli nismo uporabljali nobenih herbicidov, pesticidov ali drugih agrokemičnih in fitofarmacevtskih sredstev. Trenutno nismo v programu ekološkega certificiranja, ampak vsi naši pridelki in izdelki so dejansko »bio«.",
+  },
+  link: {
+    en: "here.",
+    sl: "tukaj.",
+  },
+}
