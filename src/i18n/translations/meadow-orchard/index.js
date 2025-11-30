51b73ba97eee3b67d4b27e8d973548b93328f990
Elijan Mastnak <git@ejmastnak.com>
2025-11-30 08:33:23 +0100

    feat: add Meadow Orchard page

--- src/i18n/translations/meadow-orchard/index.js
+++ src/i18n/translations/meadow-orchard/index.js
@@ -10,7 +10,167 @@ export default {
     },
   },
   h1: {
-    en: "Meadow orchard",
-    sl: "Travniški sadovnjak",
+    en: "What is a meadow orchard?",
+    sl: "Opredelitev travniškega sadovnjaka",
   },
+  dandelionsImgAlt: {
+    en: "Image showing a field of dandelions in focus in the foreground with old-growth apple trees in the background.",
+    sl: "Slika cvetočega regrata z jablanami v ozadju.",
+  },
+  appleBlossomsImgAlt: {
+    en: "Image showing apple trees in blossom in spring.",
+    sl: "Slika, ki kaže jablano v polnem cvetu pomladi.",
+  },
+  sliveFlowersImgAlt: {
+    en: "Image showing plum trees in bloom in early spring.",
+    sl: "Slika cvetočih češpelj zgodaj spomladi.",
+  },
+  intro: [
+    {
+      en: "Our farm consists primarily of",
+      sl: `
+Na naši kmetiji imamo visokodebelni travniški sadovnjak. Marsikoga od naših obiskovalcev in strank zanima, kaj je visokodebelni travniški sadovnjak. Gre namreč za habitat, ki ga redko najdete drugje kot na podeželju. Opišemo ga lahjo takole:
+`,
+    },
+    {
+      en: "meadow orchards",
+      sl: "",
+    },
+    {
+      en: "(sometimes also translated from Slovene as ",
+      sl: "",
+    },
+    {
+      en: "tall-trunk meadow orchards",
+      sl: "",
+    },
+    {
+      en: "). Our visitors and customers often ask what is a meadow orchard, since the habitat is rarely found outside of the farming countryside. Here is our description. ",
+      sl: "",
+    },
+  ],
+  definition: {
+    en: `Meadow orchards are a traditional agroforestry system characterized by high-growing, old-growth fruit trees scattered sparsely and irregularly throughout a grassland understory. In the name "tall-trunk meadow orchard", the meadow refers to the grassland understory, orchard to the fruit trees, and "tall-trunk" to the tree's high-growing, tall trunks (in contrast to the short trees found in conventional intensive orchards).`,
+    sl: `
+Visokodebelni travniški sadovnjak je tradicionalen sistem kultivirane krajine, za katerega so značilna visoka stara sadna drevesa, razkropljena brez strogega reda na travni podlagi. O sadovnjaku govorimo, ker gre za sadna drevesa; sadovnjak je travniški, ker drevesa rastejo na travniku; visokodebelni pa je zato, ker so drevesa v nasprotju z danes prevladujočimi intenzivnimi komercialnimi nasadi visoka – zaradi česar obiranje plodov zahteva več časa in truda, nekaterih pa sploh ne dosežemo in je treba počakati, da padejo na tla.
+`,
+  },
+  features: {
+    en: "Meadow orchards consist of mixed species of fruit trees, often grafted onto wild stock, and the understory is traditionally grazed by free-ranging livestock or mowed for use as hay.",
+    sl: `
+Na travniških sadovnjakih rastejo različne vrste sadnega drevja, včasih cepljena na divjake, travo pa ali popasejo domače živali ali pa jo pokosimo za krmo.
+`,
+  },
+  mosiac: {
+    en: "Meadow orchards are a defining feature in the mosaic of extensively-managed pastures, crop fields, hedges, and orchards characterizing the traditional farming countryside of temperate Europe, allow for integrated fruit production and grazing in otherwise marginal land, and provide valuable ecological habitat for flowers, grasses, insects, and birds.",
+    sl: `
+Travniški sadovnjaki so karakterističen sestavni del v mozaiku ekstenzivno obdelovanih pašnikov, polj in mejic. Značilni so za tradicionalno kmetijsko krajino v Evropi zmernega podnebnega pasu. Omogočajo integrirano sadjarstvo in pašništvo in so dragocen habitat za cvetnice, trave, žuželke in ptice.
+`,
+  },
+  comparisonTitle: {
+    en: "Comparison to conventional orchards:",
+    sl: "Primerjava visokodebelnega travniškega sadovnjaka s konvencionalnimi sadovnjaki:",
+  },
+  comparison: {
+    en: "the tall, old-growth trees, mixed varieties, and scattered distribution of trees in meadow orchards contrasts with conventional fruit orchards, which are characterized by monoculture plantations of short, heavily-pruned trees closely spaced in regular rows—the short trees and regular spacing in conventional orchards allow for efficient chemical treatments and mechanized harvest. Intensive orchards conventionally lack an appreciable meadow undestory and rely heavily on agrochemical application to sustain high yields. ",
+    sl: `
+Visoka in stara drevesa različnih vrst in sort, ki so razkropljena po travniku, so pravo nasprotje konvencionalnih monokulturnih sadnih nasadov, v katerih so drevesa nizka, sajena tesno skupaj v pravilnih razmakih in ravnih vrstah in močno obrezana, kar vse omogoča učinkovito mehanično obiranje in uporabo kemičnih sredstev. Intenzivni konvencionalni nasadi praviloma nimajo raznolike travne podlage, visoke donose pa zagotavlja uporaba agrokemičnih sredstev.
+`,
+  },
+  notesHeading: {
+    en: "Some notes:",
+    sl: "Še nekaj zanimivisoti:",
+  },
+  notes: {
+    species: {
+      intro: {
+        en: "Fruit and nut trees commonly found in meadow orchards in our native Koroška region include many varieties of",
+        sl: "Sadno drevje, ki uspeva v naši koroški regiji, vključuje številne sorte",
+      },
+      apple: {
+        en: "apple (Malus domestica), ",
+        sl: "jablan (malus domestica), ",
+      },
+      crabapple: {
+        en: "crabapple (Malus sylvestris), ",
+        sl: "lesnike (malus sylvestris), ",
+      },
+      pear: {
+        en: "pear (Pyrus communis), ",
+        sl: "hruške (pyrus communis), ",
+      },
+      plum: {
+        en: "plum (Prunus domestica), ",
+        sl: "slive (prunus domestica), ",
+      },
+      sweetCherry: {
+        en: "sweet cherry (Prunus avium), ",
+        sl: "češnje (prunus avium) ",
+      },
+      sourCherry: {
+        en: "sour cherry (Prunus cerasus), ",
+        sl: "in višnje (prunus cerasus), ",
+      },
+      and: {
+        en: "and ",
+        sl: "pogost pa je tudi ",
+      },
+      walnut: {
+        en: "common walnut (Juglans regia).",
+        sl: "oreh (juglans regia).",
+      },
+    },
+    integrated: {
+      en: "Meadow orchards have traditional significance as a form of integrated land management that produces both fruit and feed for livestock in marginal, steep, and otherwise difficult-to-cultivate land. Historically, the fruit provided both food for family use and a source of supplemental income when sold in market.",
+      sl: "Pomen travniških sadovnjakov je v integriranem kultiviranju zemlje, ki zajema pridelovanje sadja in krme za živino na terenih, ki so odročni, strmi in sicer zahtevni za obdelavo. Tradicionalno je sveže ali predelano sadje bilo pomembno v prehrani družine in kot vir dodatnega zaslužka.",
+    },
+    biodiversity: {
+      p1: {
+        en: "Meadow orchards have tremendous ecological value. The tall, often hollow trunks and large crowns offer rare nesting and roosting sites to hollow-dwelling birds and bats. Fungi, mosses, and lichens grow on the older trees, and the mixed grassland understory and fruit trees provide foraging sites and living habitat for insects, birds, small mammals, and reptiles. Honey bees, wild bees, bumblebees, and numerous other pollinators visit the flowers for nectar, and in turn contribute beneficially to pollination throughout the surrounding farmland; and the diverse populations of insects and birds help with pest control.",
+        sl: `
+Travniški sadovnjaki imajo izjemno ekološko vrednost. Visoka in pogosto votla debla  ter velike, razraščene krošnje nudijo zavetje in gnezdišča za ptice in netopirje. Mahovi, lišaji in gobe, ki rastejo na starih drevesih, so življenjsko okolje in hrana za žuželke, ptice, plazilce in manjše sesalce, prav tako kot sama sadna drevesa in travna podrast. Čebele, divje čebele, čmrlji in številni drugi opraševalci obletavajo cvetove in nabirajo nektar in cvetni prah, pri tem pa opravljajo nepogrešljivo opraševalno delo v sadovnjaku in širšem okolju. Raznolikost žuželk in ptic je pomembna za kontrolo škodljivcev.
+`,
+      },
+      studies: {
+        intro: {
+          en: "Numerous studies have investigated and confirmed high biodiversity in meadow orchards; see e.g.",
+          sl: "Visoko biodiverziteto travniških sadovnjakov so preučevale in potrdile številne študije. Glej npr.",
+        },
+        guariento: {
+          authors: {
+            en: "Guariento, E., et. al, 2024.",
+            sl: "Guariento, E., et. al, 2024.",
+          },
+          title: {
+            en: "Meadow orchards as a good practice example for improving biodiversity in intensive apple orchards.",
+            sl: "Meadow orchards as a good practice example for improving biodiversity in intensive apple orchards.",
+          },
+          journal: {
+            en: "Biological Conservation, Volume 299, ",
+            sl: "Biological Conservation, Volume 299, ",
+          },
+        },
+        //  , and the references therein. 
+        kajtoch: {
+          authors: {
+            en: "Kajtoch, Ł., 2017",
+            sl: "Kajtoch, Ł., 2017",
+          },
+          title: {
+            en: "The importance of traditional orchards for breeding birds: the preliminary study on central European example.",
+            sl: "The importance of traditional orchards for breeding birds: the preliminary study on central European example.",
+          },
+          journal: {
+            en: "Acta Oecol. 78, 53–60, ",
+            sl: "Acta Oecol. 78, 53–60 ",
+          },
+        },
+        andReferencesTherein: {
+          en: "and the references therein.",
+          sl: "in nadaljnje reference v omenjenih člankih.",
+        },
+
+      }
+    },
+  }
 }
