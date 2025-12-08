fc162478e968af4bb15e19716573fffbb38c749b
Elijan Mastnak <git@ejmastnak.com>
2025-12-08 12:12:34 +0100

    feat: translate contact and howtobuy pages

--- src/i18n/translations/contact/index.js
+++ src/i18n/translations/contact/index.js
@@ -9,4 +9,30 @@ export default {
       sl: "Kontakt",
     },
   },
+  h1: {
+    en: "Contact",
+    sl: "Kontakt",
+  },
+  p1: {
+    en: "You can contact us with any of the following options:",
+    sl: "Kontaktirate nas lahko na sledeče načine:",
+  },
+  phone1: {
+    en: "Phone or WhatsApp at",
+    sl: "Po telefonu na številki",
+  },
+  phone2: {
+    en: "(call or SMS, whichever you prefer) ",
+    sl: "(lahko klic ali SMS, kar vam bolj odgovarja)",
+  },
+  email: {
+    en: "Email at",
+    sl: "Po e-pošti na naslovu",
+  },
+  p2: {
+    en: ``,
+    sl: `Veseli smo osebnega kontakta.
+Z veseljem odgovorimo na morebitna vprašanja, svetujemo pri naročilih, uskladimo dostavo, itn.
+`,
+  },
 }
--- src/i18n/translations/how-to-buy/index.js
+++ src/i18n/translations/how-to-buy/index.js
@@ -9,4 +9,51 @@ export default {
       sl: "Nakup izdelkov",
     },
   },
+  h1: {
+    en: "How to buy",
+    sl: "Nakup izdelkov",
+  },
+  orderByContact: {
+    en: "Option 1: Contact us by phone or email",
+    sl: "1. opcija: Naročilo po telefonu ali e-pošti",
+  },
+  phone1: {
+    en: "Contact us directly by phone at",
+    sl: "Kontaktirate nas lahko na telefonski številki",
+  },
+  phone2: {
+    en: "(call or SMS, whichever you prefer) ",
+    sl: "(klic ali SMS, kar vam bolj odgovarja)",
+  },
+  email: {
+    en: "or by email at",
+    sl: "ali po e-pošti na naslovu.",
+  },
+  details: {
+    en: "We'll respond to confirm your order and arrange delivery.",
+    sl: "Skupaj se dogovorimo za praktične detalje naročila.",
+  },
+  orderByWebshop: {
+    en: "Option 2: Use our online shop",
+    sl: "2. opcija: Naročilo v spletni trgovini",
+  },
+  webshop: {
+    en: "If you prefer, you can also order products using our online shop:",
+    sl: "Če vam bolj odgovarja, lahko izdelke naročite v naši spletni trgovini:",
+  },
+  webshopLink: {
+    en: "Our online shop",
+    sl: "Naša spletna trgovina",
+  },
+
+  deliveryH2: {
+    en: "Delivery",
+    sl: "Dostava",
+  },
+  deliveryP: {
+    en: `We can deliver to Ljubljana and locally to the greater Mislinja region. If you are located elsewhere in Slovenia and are interested in our products, we may be able to arrange delivery for larger orders—contact us for details.`,
+    sl: `Dostavljamo po Ljubljani ter lokalno v širši mislinjski regiji. Pri večjih naročilih se lahko tudi dogovorimo za dostavo kjerkoli v Sloveniji. Kontaktirajte nas, da dorečemo praktične detalje.`,
+  },
+
+
 }
