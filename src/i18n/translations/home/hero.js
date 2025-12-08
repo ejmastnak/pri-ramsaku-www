5d43c55bb8cb14c6b19a5afedd9ac0e6163c4352
Elijan Mastnak <git@ejmastnak.com>
2025-12-08 11:28:00 +0100

    feat(wip): rough home page translations

--- src/i18n/translations/home/hero.js
+++ src/i18n/translations/home/hero.js
@@ -1,27 +1,10 @@
 export default {
-  head: {
-    title: {
-      en: "Home",
-      sl: "Domačija Ramšak",
-    },
-    description: {
-      en: "Home page",
-      sl: "Glavna stran",
-    },
-  },
-  heroSubheading: {
-    en: "(Gaia family farm)",
-    sl: "",
-  },
-  heroText: {
+  text: {
     en: "Heirloom-variety apple juice and fruit products from traditional meadow orchards. Family-run on the foothills of the Pohorje mountains in Mislinja, Slovenia.",
     sl: "Sokovi in sorodni sadni izdelki iz starih nekomercialnih sort iz lastnih travniških sadovnjakov. Družinkso in ekološko pridelano na obronkih Pohorja.",
   },
-  heroMore: {
+  more: {
     en: "About us",
     sl: "Več o nas",
   },
-  about: {
-    
-  }
 }
---
+++ src/i18n/translations/home/index.js
@@ -0,0 +1,40 @@
+export default {
+  head: {
+    title: {
+      en: "Home",
+      sl: "Domačija Ramšak",
+    },
+    description: {
+      en: "Home page",
+      sl: "Glavna stran",
+    },
+  },
+  p1: {
+    en: `We are a family farm specializing in small-scale production of apple juice, cider vinegar, and related fruit products using traditional heirloom apple varieties from Slovenia and the former Austro-Hungarian empire. We strive to make tastier, less processed, and nutrionally richer products than typical commercial equivalents.`,
+    sl: `Smo manjša družinska kmetija na slovenskem Koroškem. Po načelih ekološkega kmetovanja pridelujemo jabolčni sok, jabolčni kis in sorodne sadne izdelke iz starih sort jabolk in hrušk, ki dozorijo v našem visokodebelnem travniškem sadovnjaku, in zeliščne izdelke iz zelišč z našega travnika in iz gozda.
+        Delamo s poudarkom na uporabo starih, nekomercialnih sort ter ročno predelavo na manjši skali, da bi naši izdelki imeli bogat okus in polno hranilno vrednost.
+`,
+  },
+  p2: {
+    en: `We strive to preserve the use of heirloom varieties; traditional, small-scale production methods, and ecologically-important meadow-orchard habitat, and to contribute to the preservation and continued cultivation of the Slovene farming countryside.
+`,
+    sl: `Trudimo se, da bi ohranili uporabo starih sort, tradicionalne načine predelave, ohranili kmetijsko krajino in pomembni ekološki habitat (visokodebelni travniški sadovnjaki) prispevali k ohranitvi in slovenskega podeželja, ipd.
+`,
+  },
+  productsH2: {
+    en: "Products",
+    sl: "Izdelki",
+  },
+  productsP: {
+    en: `We sell apple juice, apple cider vinegar, jams, and dried fruits; and can also prepare other products by special arrangement.`,
+    sl: `Ponujamo jabolčni sok, jabolčni kis, in zeliščne izdelke iz zelišč z našega travnika in iz gozda. Po dogovoru lahko pripravimo tudi sorodne izdelke iz naših sadovnjakov in travnikov.`,
+  },
+  productsLink: {
+    en: "View our products",
+    sl: "Ogled naših izdelkov",
+  },
+  linksH2: {
+    en: "Links",
+    sl: "Povezave",
+  },
+}
