1324a5c7bf5727a6d4b75bc7a6a66b3cd12d4f37
Elijan Mastnak <git@ejmastnak.com>
2025-12-10 12:45:37 +0100

    style: improve hero layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,14 @@ import defaultTheme from 'tailwindcss/defaultTheme'
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
+    screens: {
+      'xs': '540px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
     extend: {
       fontFamily: {
         'sans': ['Latin Modern Sans', ...defaultTheme.fontFamily.sans],
