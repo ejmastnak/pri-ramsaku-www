f11241cff4f680f4b3c609fe593e8eb331623ccc
Elijan Mastnak <git@ejmastnak.com>
2026-05-07 13:59:22 +0200

    feat: add Slovene translations of About page

--- src/i18n/translations/about/index.js
+++ src/i18n/translations/about/index.js
@@ -23,23 +23,23 @@ export default {
   },
   priRamsakuBodyHtml: {
     en: "The name <i>Kmetija Pri Ramšaku</i> (Ramšak Farm) is our farm's traditional name since the 1800s. In the Slovenian countryside a farm typically has a \"traditional\" local name that stays with the farm regardless of the legal name of the family living there. The first people to develop the farm were named Ramšak—a common last name in the local Koroška region—and the name stayed with the farm.",
-    sl: "The name <i>Kmetija Pri Ramšaku</i> (Ramšak Farm) is our farm's traditional name since the 1800s. In the Slovenian countryside a farm typically has a \"traditional\" local name that stays with the farm regardless of the legal name of the family living there. The first people to develop the farm were named Ramšak—a common last name in the local Koroška region—and the name stayed with the farm.",
+    sl: "Ime <i>Kmetija Pri Ramšaku</i> je tradicionalno ime naše kmetije že od 19. stoletja. Na slovenskem podeželju imajo kmetije pogosto svoje »domače« oziroma tradicionalno ime, ki ostane povezano s kmetijo ne glede na uradni priimek družine, ki tam živi. Prvi gospodarji, ki so kmetijo razvili in obdelovali, so se pisali Ramšak, in ime se je ohranilo vse do danes.",
   },
   historyHeading: {
     en: "The farm's history",
-    sl: "Zgodovina domačije",
+    sl: "Zgodovina kmetije",
   },
   historyP1Html: {
     en: "Our farm was founded in the 1800s by the Ramšak family, who gave the farm its name—<i>Pri Ramšaku</i>. Throughout the 1800s and 1900s, the Ramšak family maintained a thriving homestead, raising livestock, growing grains and produce, and operating a grain mill and sawmill on the forested, mountainous terrain.",
-    sl: "Our farm was founded in the 1800s by the Ramšak family, who gave the farm its name—<i>Pri Ramšaku</i>. Throughout the 1800s and 1900s, the Ramšak family maintained a thriving homestead, raising livestock, growing grains and produce, and operating a grain mill and sawmill on the unforgiving mountainous terrain.",
+    sl: "Našo kmetijo je v 19. stoletju ustanovila stara družina Ramšak, ki je okrog leta 1800 postavila kmečko hišo, ki še vedno stoji, obdelovala polja na strmih prisojnih bregovih, gojila živino in delala v gozdu. Še pred dobrega pol stoletja je bila kmetija samooskrbna, imela žago, mlin in kovačijo, trla lan in kuhala oglje. Oče zadnjega lastnika pred nami je lovil korak z modernizacijo kmetijstva in bil med prvimi v teh krajih, ki so pridelovali mleko za oddajo.",
   },
   historyP2: {
     en: "But for lack of a younger generation to take over—as is unfortunately all too common in the rural European countryside—the farm drifted into dormancy by the 2000s. In the early 2020s, the farm changed hands to the Mastnak family, who took on and now continue operation of the farm, and whose voice you are reading on this page.",
-    sl: "But for lack of a younger generation to take over—as is unfortunately all too common in the rural European countryside—the farm drifted into dormancy by the 2000s. In the early 2020s, the farm changed hands to the Mastnak family, who revived and now continue operation of the farm, and whose voice you are reading on this page.",
+    sl: "Ker pa ni bilo mlajše generacije, ki bi prevzela delo na kmetiji, je ta do začetka 21. stoletja postopoma zamrla, kar je danes žal pogosta usoda marsikatere hribovske kmetije na slovenskem podeželju. V začetku dvajsetih let tega stoletja je kmetija prešla v roke družine Mastnak, ki je prevzela njeno obnovo in gospodarjenje ter danes nadaljuje njeno zgodbo. Pri vseh opravilih so nam s svojim znanjem in izkušnjami (in tudi s kmetijskimi stroji) v oporo in pomoč sosedje, ki so nas lepo sprejeli v svojo okolje.",
   },
   historyP3: {
     en: "We are now a small family operation and by no means professional nor full-time farmers, but have done our best to keep the land in cultivation, plant a new generation of fruit trees, and preserve the farm's long tradition and history.",
-    sl: "We are now a small family operation and by no means professional nor full-time farmers, but have done our best to keep the land in cultivation, preserve the farm's long tradition and history, and plant a new generation of fruit trees."
+    sl: "Danes smo majhna družinska kmetija in nikakor ne poklicni oziroma polno zaposleni kmetje, vendar se po svojih najboljših močeh trudimo, da zemlja ostaja obdelana in da se ohranjata dolgoletna tradicija in zgodovina kmetije. Trenutno povsvečamo veliko pozornosti pomlajanju starega drevja in zasajanju nove generacije sadnega drevja, zlasti starih sort jablan in hrušk, ki uspevajo v Mislinjski dolini in na Koroškem."
   },
 
   varietiesHeading: {
@@ -48,11 +48,11 @@ export default {
   },
   varietiesP1: {
     en: "Since taking over the farm, we have emphasized using heirloom apple varieties grown in our tall-trunk meadow orchards. Heirloom apples grow less abundantly and consistently than the modern varieties grown on large-scale commercial farms, but they also tend to be more flavorful, aromatic, and visually interesting than conventional apples.",
-    sl: "Od kar smo prevzeli kmetijo poudarjamo uporabo starih, tradicionalnih sort jabolk, ki že dolga leta rastejeo v naših visokodebelnih travniških sadovnjakih. Te stare sorte rodijo manj obilno in zanesljivo kot moderne komercialne sorte, ki jih pridelujejo na večjih sadjarskih kmetijah (zato jih redkokdaj zasledimo v konvencionalnih živilskih trgovinah), ampak so tipično bolj okusna in aromatična, kot konvencionalne komercialne sorte.",
+    sl: "Od kar smo prevzeli kmetijo poudarjamo uporabo starih, tradicionalnih sort jabolk, ki že dolga leta rastejo v naših visokodebelnih travniških sadovnjakih. Te stare sorte rodijo manj obilno in zanesljivo kot moderne komercialne sorte, ki jih pridelujejo na večjih sadjarskih kmetijah (zato jih redkokdaj zasledimo v konvencionalnih živilskih trgovinah). A hkrati bolje okusne, aromatične in vizualno zanimive, kot konvencionalne komercialne sorte.",
   },
   varietiesP2: {
-    en: "We grow these varieites motivated by the practical reality that we are a small-scale, family operation and not full-time farmers with the manpower and equipment needed to maintain a conventional commercial orchard. Because our production is limited, we prefer to offer interesting, unconventional products than try to compete on a basis of scale or price with high-volume commercial farms.",
-    sl: "We grow these varieites motivated by the practical reality that we are a small-scale, family operation and not full-time farmers with the manpower and equipment needed to maintain a conventional commercial orchard. Because our production is limited, we prefer to offer interesting, unconventional products than try to compete on a basis of scale or price with high-volume commercial farms.",
+    en: "We grow these varieites both for their beauty, diversity, and distinctive characteristics; and motivated by the practical reality that we are a small-scale, family operation and not full-time farmers with the manpower and equipment needed to maintain a conventional commercial orchard. Because our production is limited, we prefer to offer interesting, unconventional products than try to compete on a basis of scale or price with high-volume commercial farms.",
+    sl: "Te sorte gojimo zaradi njihove lepote, raznolikosti in posebnih značilnosti, pa tudi zato, ker smo majhna družinska kmetija in ne poklicni sadjarji z mehanizacijo ter delovno silo, ki sta potrebni za vzdrževanje sodobnega intenzivnega sadovnjaka. Ker je naša pridelava omejena, raje ponujamo zanimive in nekoliko drugačne izdelke, namesto da bi skušali po količini ali ceni tekmovati z velikimi komercialnimi sadjarskimi kmetijami.",
   },
 
   picturesHeading: {
@@ -61,7 +61,7 @@ export default {
   },
   picturesParagraph: {
     en: "We have made an effort to document and photograph the apple and pear varieties on our farm, which you can view in the links below:",
-    sl: "Od kar smo prevzeli domačijo se trudimo, da bi poslikali in opisali sorte jabolk in hrušk, ki rastejo na kmetiji. Opise in slike si lahko ogledate na sledečih povezavah:",
+    sl: "Od kar smo prevzeli kmetijo se trudimo, da bi poslikali in opisali sorte jabolk in hrušk, ki rastejo na kmetiji. Opise in slike si lahko ogledate na sledečih povezavah:",
   },
 
   meadowOrchardHeading: {
